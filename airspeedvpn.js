9477534d7d4a63443d9ab5cc501cb9534cad76e6
puss3888 <54200632+puss3888@users.noreply.github.com>
2021-01-03 00:40:48 +0800

    Create airspeedvpn.js

---
+++ airspeedvpn.js
@@ -0,0 +1,15 @@
+var body = $response.body; 
+var obj = JSON.parse(body);
+
+$response.statusCode = 200;
+obj.userid = "5e8b22ad80ac550b214fb9d3";
+obj.expire = 9999999999999;
+obj.username = "Fly";
+obj.type = "gold";
+obj.verify = true;
+obj.email = "nobody@qq.com";
+obj.token =  "1e132dc23df9d4e3a75df4066313a7b2";
+ obj.publickey = "307b87e5e2deb035";
+
+body = JSON.stringify(obj); 
+$done(body);
