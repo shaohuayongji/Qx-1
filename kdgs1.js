3529c73eee0c0820d8a1dcc7289e9e2fa4da9675
puss3888 <54200632+puss3888@users.noreply.github.com>
2021-01-04 02:51:30 +0800

    Update and rename kdgs.js to kdgs1.js

--- kdgs1.js
+++ kdgs1.js
@@ -3,7 +3,7 @@ var obj = JSON.parse(modifiedBody);
 
 obj.data.is_vip = 1;
 obj.data.is_subscribe = 1;
-obj.daya.vip_valid_ts = "2222-2-22 22:22:22";
+obj.data.vip_valid_ts = "2222-2-22 22:22:22";
 
 modifiedBody = JSON.stringify(obj); 
 $done(modifiedBody);
