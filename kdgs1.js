1e3f1a533d3f1e627d6f710134f978af8f56ea3c
puss3888 <54200632+puss3888@users.noreply.github.com>
2021-01-04 04:45:24 +0800

    Update kdgs1.js

--- kdgs1.js
+++ kdgs1.js
@@ -2,7 +2,7 @@ var modifiedBody = $response.body;
 var obj = JSON.parse(modifiedBody);
 
 obj.data.is_vip = 1;
-obj.data.is_subscribe = 1;
+//obj.data.is_subscribe = 1;
 obj.data.vip_valid_ts = "2222-2-22 22:22:22";
 
 modifiedBody = JSON.stringify(obj); 
