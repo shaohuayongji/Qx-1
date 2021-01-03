167111f74f811355331231fb073cc4bbab1c7482
puss3888 <54200632+puss3888@users.noreply.github.com>
2021-01-03 10:23:21 +0800

    Update airspeedvpn.js

--- airspeedvpn.js
+++ airspeedvpn.js
@@ -1,6 +1,9 @@
-var body = $response.body; 
-var obj = JSON.parse(body);
+var modifiedBody = $response.body; 
+var obj = JSON.parse(modifiedBody);
+var modifiedHeaders = $response.headers;
+var modifiedStatus = 'HTTP/1.1 200 OK'
 
+//$response.statusCode = 200;
 obj.userid = "5e8b22ad80ac550b214fb9d3";
 obj.expire = 9999999999999;
 obj.username = "Fly";
@@ -10,6 +13,7 @@ obj.email = "nobody@qq.com";
 obj.token =  "1e132dc23df9d4e3a75df4066313a7b2";
  obj.publickey = "307b87e5e2deb035";
 
-body = JSON.stringify(obj); 
+modifiedBody = JSON.stringify(obj); 
+//$done(body);
 
-$done(body);
+$done({body: modifiedBody, headers: modifiedHeaders, status: modifiedStatus})
