002585a96418ea85cbc8b13b46be2973b6786a07
puss3888 <54200632+puss3888@users.noreply.github.com>
2021-01-04 20:56:02 +0800

    Delete kdgs2.js

--- kdgs2.js
+++
@@ -1,9 +0,0 @@
-var modifiedBody = $response.body; 
-var obj = JSON.parse(modifiedBody);
-
-obj.vip = 1;
-obj.is_buy_vip = true;
-obj.vip_valid_ts = "2222-2-22 22:22:22";
-
-modifiedBody = JSON.stringify(obj); 
-$done(modifiedBody);
