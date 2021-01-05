374d29fd625458b3cd009558094a3600acd78a2f
puss3888 <54200632+puss3888@users.noreply.github.com>
2021-01-05 10:02:05 +0800

    Update and rename airspeedvpn.js to asv.js

--- asv.js
+++ asv.js
@@ -1,9 +1,17 @@
+/*
+airspeedvpn
+
+[rewrite_local]
+^https:\/\/api\.express-api\.com\/v1\/(device|connect) url script-response-body https://raw.githubusercontent.com/puss3888/Qx/main/asv.js
+
+[mitm]
+hostname = api.express-api.com
+*/
+
 var modifiedBody = $response.body; 
 var obj = JSON.parse(modifiedBody);
 var modifiedHeaders = $response.headers;
 var modifiedStatus = 'HTTP/1.1 200 OK'
-
-//$response.statusCode = 200;
 obj.userid = "5e8b22ad80ac550b214fb9d3";
 obj.expire = 9999999999999;
 obj.username = "Fly";
@@ -14,6 +22,5 @@ obj.token =  "1e132dc23df9d4e3a75df4066313a7b2";
  obj.publickey = "307b87e5e2deb035";
 
 modifiedBody = JSON.stringify(obj); 
-//$done(body);
 
 $done({body: modifiedBody, headers: modifiedHeaders, status: modifiedStatus})
