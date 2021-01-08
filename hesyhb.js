bfb665491a8d1c52ea149708c3d7fb234972b92e
puss3888 <54200632+puss3888@users.noreply.github.com>
2021-01-09 00:07:54 +0800

    Update and rename hesyhb to hesyhb.js

---
+++ hesyhb.js
@@ -0,0 +1,17 @@
+/*
+洪恩双语绘本
+[rewrite_local]
+^https:\/\/bookapi\.ihuman\.com\/v1\/get_enlightening_book_info url script-response-body https://github.com/puss3888/Qx/blob/main/hesyhb.js
+^https:\/\/bookapi\.ihuman\.com\/v3\/get_book_info url script-response-body https://github.com/puss3888/Qx/blob/main/hesyhb.js
+
+[mitm]
+hostname = bookapi.ihuman.com
+*/
+
+
+
+var modifiedBody = $response.body; 
+
+modifiedBody.replace(/is_vip":1/g, 'is_vip":0');
+
+$done(modifiedBody);
