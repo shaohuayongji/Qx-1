d43f97646ccecc8700a345eac770e2766bf22b74
puss3888 <54200632+puss3888@users.noreply.github.com>
2021-01-09 01:37:38 +0800

    Update hesyhb.js

--- hesyhb.js
+++ hesyhb.js
@@ -1,8 +1,8 @@
 /*
 洪恩双语绘本
 [rewrite_local]
-^https:\/\/bookapi\.ihuman\.com\/v1\/get_enlightening_book_info url script-response-body https://github.com/puss3888/Qx/blob/main/hesyhb.js
-^https:\/\/bookapi\.ihuman\.com\/v3\/get_book_info url script-response-body https://github.com/puss3888/Qx/blob/main/hesyhb.js
+^https:\/\/bookapi\.ihuman\.com\/v1\/get_enlightening_book_info url script-response-body https://raw.githubusercontent.com/puss3888/Qx/main/hesyhb.js
+^https:\/\/bookapi\.ihuman\.com\/v3\/get_book_info url script-response-body https://raw.githubusercontent.com/puss3888/Qx/main/hesyhb.js
 
 [mitm]
 hostname = bookapi.ihuman.com
