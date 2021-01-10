b84976a5dccd9a4a432d893f5152bc1ef19ab1d8
puss3888 <54200632+puss3888@users.noreply.github.com>
2021-01-10 22:20:20 +0800

    Create hesyhb.js

---
+++ hesyhb.js
@@ -0,0 +1,17 @@
+/*
+洪恩双语绘本
+[rewrite_local]
+^https:\/\/bookapi\.ihuman\.com\/v1\/get_enlightening_book_info url script-response-body https://raw.githubusercontent.com/puss3888/Qx/main/hesyhb.js
+^https:\/\/bookapi\.ihuman\.com\/v3\/get_book_info url script-response-body https://raw.githubusercontent.com/puss3888/Qx/main/hesyhb.js
+[mitm]
+hostname = bookapi.ihuman.com
+*/
+
+
+var modifiedBody = $response.body; 
+var obj = JSON.parse(modifiedBody);
+obj.result.books.forEach(function (item, idnex, array) {
+    item.is_vip = 0;
+})
+modifiedBody = JSON.stringify(obj); 
+$done(modifiedBody);
