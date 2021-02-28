67359ac556cf0e9565217b79fb87f1556cb4c509
puss3888 <54200632+puss3888@users.noreply.github.com>
2021-02-28 16:31:33 +0800

    Update txbpy.js

--- txbpy.js
+++ txbpy.js
@@ -1,9 +1,10 @@
 /*
 兔小贝拼音
 
-https:\/\/(.+)-api\.tuxiaobei\.com\/v1\/user\/ url script-response-body https://raw.githubusercontent.com/puss3888/Qx/main/txbpy.js
+https:\/\/(\.+)-api\.tuxiaobei\.com\/v1\/user\/ url script-response-body https://raw.githubusercontent.com/puss3888/Qx/main/txbpy.js
 *-api.tuxiaobei.com
 */
 
-body = $response.body.replace(/vip":0/g, 'vip":1').replace(/vip_expire_at":null/g, 'vip_expire_at":"1800000000"').replace(/vip_type":""/g, 'vip_type":"2"');
-$done({body});
+var modifiedBody = $response.body; 
+modifiedBody = modifiedBody.replace(/vip":0/g, 'vip":1').replace(/vip_expire_at":null/g, 'vip_expire_at":"1800000000"').replace(/vip_type":""/g, 'vip_type":"2"');
+$done(modifiedBody);
