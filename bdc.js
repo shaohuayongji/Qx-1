f72c422898c4f7b880dbea25fa590ad29aa492cc
puss3888 <54200632+puss3888@users.noreply.github.com>
2021-03-06 13:45:10 +0800

    Update bdc.js

--- bdc.js
+++ bdc.js
@@ -1,8 +1,5 @@
 /*
-Baidu netdisc unlocks online video play speed.
-Key data from thor filter
-Surge4.0:
-http-response https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/BaiduCloud.js
+bai du bei su
 
 QX1.0.0:
 https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/BaiduCloud.js
